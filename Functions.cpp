d4283c476555459e81c32de2be4456a7f7821b18
erprincechauhan <erprincechauhan@gmail.com>
2026-05-28 05:06:17 +0530

    learned about functions

---
+++ Functions.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    void printNumber() {
+        int a;
+
+        cout << "Enter the number: ";
+        cin >> a;
+
+        cout << a;
+
+    }
+};
+
+int main (){
+    Solution a;
+    a.printNumber();
+}
+
