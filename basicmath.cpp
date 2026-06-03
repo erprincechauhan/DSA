258c1e55f58509a43f3b860bcbb0d4b023880763
erprincechauhan <erprincechauhan@gmail.com>
2026-06-03 13:41:07 +0530

    Solved palindrome

--- basicmath.cpp
+++ basicmath.cpp
@@ -29,13 +29,32 @@ int reverse(int n){
     return reversed;
 }
 
+// You are given an integer n. You need to check whether the number is a palindrome number or not. Return true if it's a palindrome number, otherwise return false.
+bool palindrome(int n){
+    int num = n;
+    int last_dig, reversed = 0;
+    bool flag ;
+
+    while (n != 0){
+        last_dig = n % 10;
+        reversed = (reversed * 10) + last_dig;
+        n = n /10;
+    }
+
+    if (num == reversed) return flag = true;
+    else return flag = false;
+
+}
+
+
 int main (){
     int num;
     cout << "Enter the number: ";
     cin >> num ;
 
     // countNumber(num);
-    reverse(num);
+    // reverse(num);
+    palindrome(num);
     
 
     return 0;
