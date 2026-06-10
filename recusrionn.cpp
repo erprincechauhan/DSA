823081d7923a498e2e830e787efd1612b4f2c0a2
erprincechauhan <erprincechauhan@gmail.com>
2026-06-10 19:12:04 +0530

    Add recursive solutions for sum of N numbers and factorial

--- recusrionn.cpp
+++ recusrionn.cpp
@@ -16,12 +16,22 @@ void print1ton(int n){
 
 }
 
+int NumbersSum(int n){
+    if (n == 0 ) return 0;
+    return n + NumbersSum(n-1);
+}
+
+int factorial(int n){
+    if (n == 0 ) return 1;
+    return n * factorial(n-1); 
+}
+    
 int main (){
     int n ;
     cout << "Enter the numbers: ";
 
     cin >> n;
     // printnto1(n);
-    print1ton(n);
-
+    // print1ton(n);
+    NumbersSum(n);
 }
