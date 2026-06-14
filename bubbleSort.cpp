61d076a691120dbc3b0418343894584e86ef9ebb
erprincechauhan <erprincechauhan@gmail.com>
2026-06-14 09:11:45 +0530

    Implement bubble sort algorithm

---
+++ bubbleSort.cpp
@@ -0,0 +1,45 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[],int n){
+    for (int i = 0 ; i < n ; i++){
+        for (int j = 0 ; j < n-i-1 ; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+
+int main(){
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements of array: " << endl;
+    for (int i = 0 ; i < n ;i++ ){
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr,n);
+    
+    for (int i = 0 ; i < n ;i++ ){
+        cout << arr[i] << " ";
+    }
+    
+}
+
+""" 
+Time Complexity = O(n^2)
+This complexity is for average and worst case.
+for best case O(n).
+"""
+
+
+
+
