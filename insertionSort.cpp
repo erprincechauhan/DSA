52bd10929a6fe511dcc092eb87b1d3ed4283e8e8
erprincechauhan <erprincechauhan@gmail.com>
2026-06-16 15:52:43 +0530

    Implement insertion sort algorithm

---
+++ insertionSort.cpp
@@ -0,0 +1,39 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(int arr[], int n){
+    for (int i = 0 ; i < n; i++){
+        int key = arr[i] , j = i-1;
+        while(j >= 0 && arr[j]>key){
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
+    }
+
+}
+
+
+int main(){
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements of array: " << endl;
+    for (int i = 0 ; i < n ;i++ ){
+        cin >> arr[i];
+    }
+
+    insertionSort(arr,n);
+    
+    for (int i = 0 ; i < n ;i++ ){
+        cout << arr[i] << " ";
+    }
+    
+    
+    return 0;
+}
+
+
